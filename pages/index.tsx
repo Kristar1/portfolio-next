3bf2f566c3de150a6ae637f66920e73eb9ff78ff
Krish Kedia <kediak32@gmail.com>
2022-10-13 21:16:05 +0530

    rebuild

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
-import Hero2 from "../components/home/Hero2";
+import Hero2 from "../components/home/Hero2.jsx";
 import Process from "./../components/home/Process";
 import Projects from "../components/home/Projects";
 import Cta from "../components/home/Cta";
